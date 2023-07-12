998e38919ecc64f28e8aaf37244c3b2c6a9ada1a
David Koller <d.koller@tarent.de>
2023-07-12 13:01:13 +0200

    init

---
+++ src/accessory.ts
@@ -0,0 +1,68 @@
+import { API, AccessoryPlugin, AccessoryConfig, HAP, Logger, Service, CharacteristicEventTypes, CharacteristicGetCallback, CharacteristicSetCallback, CharacteristicValue } from 'homebridge';
+
+export = (api: API) => {
+  api.registerAccessory("homebridge-web-valve", V);
+};
+
+class V implements AccessoryPlugin {
+  private readonly log: Logger;
+  private readonly valveService: Service;
+  private readonly informationService: Service;
+  private readonly config: AccessoryConfig;
+  private readonly hap: HAP;
+
+  constructor(log: Logger, config: AccessoryConfig, api: API) {
+    this.log = log;
+    this.config = config;
+    this.hap = api.hap;
+
+    this.valveService = new this.hap.Service.Valve(V.name);
+
+    this.informationService = new this.hap.Service.AccessoryInformation()
+      .setCharacteristic(this.hap.Characteristic.Manufacturer, "David Koller")
+      .setCharacteristic(this.hap.Characteristic.Model, "Irrigation Valve v1")
+      .setCharacteristic(this.hap.Characteristic.SerialNumber, "420");
+
+    this.valveService.setCharacteristic(this.hap.Characteristic.ValveType, 2);
+
+    this.valveService.getCharacteristic(this.hap.Characteristic.Active)
+      .on(CharacteristicEventTypes.GET, this.handleActiveGet.bind(this))
+      .on(CharacteristicEventTypes.SET, this.handleActiveSet.bind(this));
+
+    this.valveService.getCharacteristic(this.hap.Characteristic.InUse)
+      .on(CharacteristicEventTypes.GET, this.handleInUseGet.bind(this));
+
+    this.valveService.getCharacteristic(this.hap.Characteristic.ValveType)
+      .on(CharacteristicEventTypes.GET, this.handleValveTypeGet.bind(this));
+  }
+
+  handleActiveGet(callback: CharacteristicGetCallback) {
+    this.log.debug('Triggered GET Active');
+    const currentValue = this.hap.Characteristic.Active.INACTIVE;
+    callback(null, currentValue);
+  }
+
+  handleActiveSet(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+    this.log.debug('Triggered SET Active:', value);
+    callback(null);
+  }
+
+  handleInUseGet(callback: CharacteristicGetCallback) {
+    this.log.debug('Triggered GET InUse');
+    const currentValue = this.hap.Characteristic.InUse.NOT_IN_USE;
+    callback(null, currentValue);
+  }
+
+  handleValveTypeGet(callback: CharacteristicGetCallback) {
+    this.log.debug('Triggered GET ValveType');
+    const currentValue = this.hap.Characteristic.ValveType.GENERIC_VALVE;
+    callback(null, currentValue);
+  }
+
+  getServices(): Service[] {
+    return [
+      this.informationService,
+      this.valveService
+    ];
+  }
+}
