35d2fa1a01de9b696d06d8e72bd40129b421e4f2
David Koller <d.koller@tarent.de>
2023-07-12 22:10:47 +0200

    changed back to valve of type irrigation

--- src/accessory.ts
+++ src/accessory.ts
@@ -21,7 +21,7 @@ class V implements AccessoryPlugin {
     this.active = this.hap.Characteristic.Active.INACTIVE
     this.inUse = this.hap.Characteristic.InUse.NOT_IN_USE
 
-    this.valveService = new this.hap.Service.IrrigationSystem(V.name);
+    this.valveService = new this.hap.Service.Valve(V.name);
 
     this.informationService = new this.hap.Service.AccessoryInformation()
       .setCharacteristic(this.hap.Characteristic.Manufacturer, "David Koller")
@@ -35,6 +35,9 @@ class V implements AccessoryPlugin {
     this.valveService.getCharacteristic(this.hap.Characteristic.InUse)
       .on(CharacteristicEventTypes.GET, this.handleInUseGet.bind(this))
       .on(CharacteristicEventTypes.SET, this.handleInUseSet.bind(this));
+
+    this.valveService.getCharacteristic(this.hap.Characteristic.ValveType)
+      .on(CharacteristicEventTypes.GET, this.handleValveTypeGet.bind(this));
   }
 
   handleActiveGet(callback: CharacteristicGetCallback) {
@@ -68,6 +71,12 @@ class V implements AccessoryPlugin {
       this.active = this.hap.Characteristic.InUse.NOT_IN_USE
     }
     callback(null)
+  }
+
+  handleValveTypeGet(callback: CharacteristicGetCallback) {
+    this.log.debug('Triggered GET ValveType');
+    const currentValue = this.hap.Characteristic.ValveType.IRRIGATION ;
+    callback(null, currentValue);
   }
 
   getServices(): Service[] {
